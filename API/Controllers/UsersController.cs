cb191cd3b4bc4135647a35387cedc4594a8c1907
Igor Cancar <icancar99@gmail.com>
2023-09-30 12:20:25 +0200

    Add Register method

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,9 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")] //https://localhost:5001/api/users
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _dataContext;
         public UsersController(DataContext context)
