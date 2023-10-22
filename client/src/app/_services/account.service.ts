c3d72a19345bb53e4c6a7dcf91078d238572cb82
Igor Cancar <icancar99@gmail.com>
2023-10-22 14:17:15 +0200

    Add register

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -34,4 +34,16 @@ export class AccountService { //instatiated when application is created and dele
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
   }
+
+  register(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if(user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        return user;
+      })
+    );
+  }
 }
