a5bfa952211d175e66e14d571fda5616a6d9abed
Igor Cancar <icancar99@gmail.com>
2023-10-22 13:01:46 +0200

    Implement registration form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export default class RegisterComponent implements OnInit{
+
+  model: any = {}
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    console.log('Cancelled');
+  }
+
 
 }
