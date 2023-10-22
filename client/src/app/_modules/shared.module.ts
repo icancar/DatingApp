f0ba20d8c570f626a28ae0597a9292697f4366b3
Igor Cancar <icancar99@gmail.com>
2023-10-22 19:30:51 +0200

    Add shared modules to separate module to cleanup app modules

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: "toast-bottom-right",
+      progressBar: true,
+      tapToDismiss: true,
+      closeButton: true,
+      countDuplicates: true
+    }),
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule
   ]
 })
 export class SharedModule { }
