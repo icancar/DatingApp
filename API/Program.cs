8443dd2443041e53f6b7d3a96c686a9943a37543
Igor Cancar <icancar99@gmail.com>
2023-10-25 09:16:59 +0200

    Add use of Exception Middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API.Extensions;
+using API.Middleware;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("http://localhost:4200"));
