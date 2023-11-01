a112d025ec5b9fd84a7b2dff721fb38319190e1f
Igor Cancar <icancar99@gmail.com>
2023-11-01 07:50:34 +0100

    Configure automapper

--- API/DTOs/MemberDTO.cs
+++ API/DTOs/MemberDTO.cs
@@ -3,6 +3,7 @@ namespace API.DTOs
     public class MemberDTO
     {
         public string UserName { get; set; }
+        public string PhotoUrl { get; set; }
         public int Id { get; set; }
         public int Age { get; set; }    
         public string KnownAs { get; set; }
