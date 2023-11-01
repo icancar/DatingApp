6b0eb0cce5cb50715d3da8ef0b3beda0f8b633c3
Igor Cancar <icancar99@gmail.com>
2023-11-01 07:45:42 +0100

    Implement use of automapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,22 +13,32 @@ namespace API.Controllers
     public class UsersController : BaseApiController
     {
         private readonly IUserRepository _userRepository;
-        public UsersController(IUserRepository userRepository)
+        private readonly IMapper _mapper;
+
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
             _userRepository = userRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-           return Ok(await _userRepository.GetUsersAsync());
+           var users = await _userRepository.GetUsersAsync();
+
+           var usersToReturn = _mapper.Map<IEnumerable<MemberDTO>>(users);
+
+           return Ok(usersToReturn);
       
         }
 
         [HttpGet("{username}")] //api/users/lisa
-        public async Task<ActionResult<AppUser>> GetUser(string username)
+        public async Task<ActionResult<MemberDTO>> GetUser(string username)
         {
-            return await _userRepository.GetUserByUsernameAsync(username);
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            return _mapper.Map<MemberDTO>(user);
+
         }
     }
 }
